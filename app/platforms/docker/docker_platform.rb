4c35dfcb905962b974402fcb229319a949d2aace
efirestone <efirestone@users.noreply.github.com>
2020-07-12 23:12:41 -0500

    Use repository names for Docker config rather than repository names

--- app/platforms/docker/docker_platform.rb
+++ app/platforms/docker/docker_platform.rb
@@ -8,12 +8,12 @@ require_relative 'remote_docker_images_list.rb'
 
 class DockerDeviceConfig < DeviceConfig
 
-  attr_reader :monitored_repositories, :username
+  attr_reader :monitored_containers, :username
 
   def initialize(config)
     super(config)
 
-    @monitored_repositories = config['monitored_repositories']
+    @monitored_containers = config['monitored']
     @username = config['username']
 
     raise "Version check definition for platform '#{DockerPlatform.name}' does not include a 'username'" if @username == nil
@@ -34,30 +34,30 @@ class DockerPlatform < Platform
   end
 
   def payload_factories
-    images = LocalDockerContainersList.new(
+    containers = LocalDockerContainersList.new(
       @device_config.username,
       @device_config.host,
       @global_config.ssh.command_line_params
     ).get_containers_list
 
-    monitored_repositories = @device_config.monitored_repositories
+    monitored_containers = @device_config.monitored_containers
 
     payload_factories = []
-    images.each do |image|
-      repository = image.repository.dup
-      tag = image.tag.dup
+    containers.each do |container|
+      name = container.name.dup
+      tag = container.tag.dup
       next if tag.nil? || tag.empty?
 
-      # Ignore unmonitored repositories
-      next unless monitored_repositories.nil? || monitored_repositories.empty? || monitored_repositories.include?(repository)
+      # Ignore unmonitored containers
+      next unless monitored_containers.nil? || monitored_containers.empty? || monitored_containers.include?(name)
 
-      info = DockerImageDeviceCheck.new(image).get_info
-      image_topic = @device_config.topic
-        .gsub('{{repository}}', repository)
+      info = DockerImageDeviceCheck.new(container).get_info
+      container_topic = @device_config.topic
+        .gsub('{{container}}', name)
         .gsub('{{tag}}', tag)
-      unique_id = "docker_#{repository.gsub('/', '_')}_#{tag}"
+      unique_id = "docker_#{name}_#{tag}"
 
-      payload_factories << DeviceMqttPayloadFactory.new(image_topic, info, unique_id)
+      payload_factories << DeviceMqttPayloadFactory.new(container_topic, info, unique_id)
     end
 
     payload_factories
