739564ea1877c44ee5afb5cb37992b458ce0998b
efirestone <efirestone@users.noreply.github.com>
2020-07-26 16:11:13 -0500

    Use /data instead of /config directory
    This makes the image compatible as a Hass add-on. Also print out the SSH public keys so they can be added to external devices without needing to get access to the internal Docker volume to read them.

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,32 +1,49 @@
 #!/bin/sh
 
-ED25519_SSH_KEY=/config/.ssh/id_ed25519
-RSA_SSH_KEY=/config/.ssh/id_rsa
+ED25519_SSH_KEY=/data/.ssh/id_ed25519
+RSA_SSH_KEY=/data/.ssh/id_rsa
 
 if [ ! -f $ED25519_SSH_KEY ]; then
   mkdir -p "$(dirname "$ED25519_SSH_KEY")"
   ssh-keygen -o -a 100 -t ed25519 -f $ED25519_SSH_KEY -N "" -q
 
-  echo ""
-  echo "New ed25519 SSH key generated. Add this key to your devices to allow Version Checker to authenticate to them. For each device, run this command:"
-  echo "    ssh-copy-id -i </volume/mounted/config>/.ssh/id_ed25519 <user>@<device_host>"
-  echo ""
+  echo " "
+  echo "New ed25519 SSH key generated."
+  echo " "
 fi
 
 if [ ! -f $RSA_SSH_KEY ]; then
   mkdir -p "$(dirname "$RSA_SSH_KEY")"
   ssh-keygen -o -a 100 -t rsa -f $RSA_SSH_KEY -N "" -q
 
-  echo ""
-  echo "New RSA SSH key generated. The ed25519 key should be preferred over this one if a device supports it. Add this key to devices to allow Version Checker to authenticate to them. For each device, run this command:"
-  echo "    ssh-copy-id -i </volume/mounted/config>/.ssh/id_rsa <user>@<device_host>"
-  echo ""
+  echo " "
+  echo "New RSA SSH key generated."
+  echo " "
 fi
 
 eval "$(ssh-agent -s)"
 ssh-add $ED25519_SSH_KEY
 ssh-add $RSA_SSH_KEY
 
-CONFIG_FILE=/config/configuration.yaml
+echo " "
+echo "To configure a device to use the ed25519 SSH key (preferred) use the following command:"
+echo " "
+echo "   echo \"$(cat "${ED25519_SSH_KEY}.pub")\" | ssh <user>@<device-hostname> 'cat >> .ssh/authorized_keys'"
+
+echo " "
+echo "In rare cases a device does not support ed25519 keys. For those devices you can configure an RSA SSH key with the following command:"
+echo " "
+echo "   echo \"$(cat "${RSA_SSH_KEY}.pub")\" | ssh <user>@<device-hostname> 'cat >> .ssh/authorized_keys'"
+
+echo " "
+echo "Some devices may require the SSH keys to be configured using their respective configuration dashboards."
+
+echo " "
+
+CONFIG_FILE=/data/configuration.yaml
+if [ ! -f $CONFIG_FILE ]; then
+  # Try the config file for Home Assistant add-ons.
+  CONFIG_FILE=/data/options.json
+fi
 
 ruby /app/version_checker.rb "$CONFIG_FILE"
